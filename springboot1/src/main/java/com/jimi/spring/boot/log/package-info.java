9e8007730524d74cee13741006b4f018d6564163
xianyao.ye <xianyao.ye@ucarinc.com>
2020-07-21 15:55:31 +0800

    spring boot log

---
+++ springboot1/src/main/java/com/jimi/spring/boot/log/package-info.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * Log Format
+ * • Date and Time: Millisecond precision and easily sortable.
+ * • Log Level: ERROR, WARN, INFO, DEBUG, or TRACE.
+ * • Process ID.
+ * • A --- separator to distinguish the start of actual log messages.
+ * • Thread name: Enclosed in square brackets (may be truncated for console output).
+ * • Logger name: This is usually the source class name (often abbreviated).
+ * • The log message
+ *
+ * application.properties   debug=true
+ * run arguments java -jar myapp.jar --debug
+ *
+ * <b>File Output</b>
+ * application.properties
+ *  logging.file / logging.path
+ *
+ * <b>Log leverls</b>
+ * logging.level.root=WARN
+ * logging.level.org.springframework.web=DEBUG
+ * logging.level.org.hibernate=ERROR
+ *
+ * @author jimi
+ * @date 2020/7/21 15:43
+ * @version 1.0
+ */
+package com.jimi.spring.boot.log;
